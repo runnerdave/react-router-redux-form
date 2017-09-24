dc545c7a7efe27e25a3886cfbb0e06ab996438b1
runnerdave <runnerdave@gmail.com>
2017-09-24 22:12:24 +1000

    retrieval of single posts as well as submission of new ones

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
+export const CREATE_POST = 'create_posts';
+
 const BASE_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=75c54aa8-1092-491c-9fe2-469e0865884f';
 
@@ -11,4 +14,23 @@ export function fetchPosts() {
         type: FETCH_POSTS,
         payload: request
     };
+}
+
+export function createPost(values, callback) {
+    const request = axios.post(`${BASE_URL}/posts${API_KEY}`, values)
+        .then(() => callback());
+
+    return {
+        type: CREATE_POST,
+        payload: request
+    }
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${BASE_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: FETCH_POST,
+        payload: request
+    }
 }
